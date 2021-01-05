9b545ffbd1c0c4f7cefd8bdc01199eeef427fab5
YoStM <yo.stm@outlook.com>
2021-01-05 13:15:20 +0100

    Add RegistrationType.php registration.html.twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class MainController extends AbstractController
 {
@@ -15,6 +20,36 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * This function allows the user to register on the website
+     * The user is redirected to the registration form
+     * Once the form has been correctly filled out, the user is redirected to the home page  ????????????????????
+     * 
+     *  @Route("/inscription", name="main_registration")
+     */
+    public function register(Request $req, EntityManagerInterface $emi, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = new User();
+
+        $registrationForm = $this->createForm(RegistrationType::class, $user);
+        $registrationForm->handleRequest($req);
+
+        if ($registrationForm->isSubmitted() && $registrationForm->isValid()) {
+            $hashed = $encoder->encodePassword($user, $user->getPassword());
+
+            $emi->persist($user);
+            $emi->flush();
+
+            $this->addFlash('success', 'Félicitations ! Vous êtes maintenant membre de notre troupe de petit ouistiti. Encore quelques informations à nous confier et vous pourrez enfin travailler avec vos semblables.');
+
+            $this->redirectToRoute('main_home');
+        }
+
+        return $this->render('main/registration.html.twig', [
+            'registrationForm' => $registrationForm->createView()
         ]);
     }
 }
